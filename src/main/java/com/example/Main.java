c7731e3622a24f5a45078b587a9b77d19545eea4
RodrigoMiguelBaptist <37610@esjaloures.og>
2026-02-25 11:38:45 +0000

    Atualizar tudo.

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,4 +1,4 @@
-package com.example;
+package src.main.java.com.example;
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/com/example/Pessoa.java
+++ src/main/java/com/example/Pessoa.java
@@ -1,4 +1,4 @@
-package com.example;
+package src.main.java.com.example;
 
 public class Pessoa {
     private String nome;
